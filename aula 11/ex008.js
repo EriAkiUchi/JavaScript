5bbe6426fe6ff98fa74f643701baed5abf4f235a
Eric Uchiyamada <94193630+EriAkiUchi@users.noreply.github.com>
2023-08-09 14:27:53 -0300

    Add files via upload

---
+++ aula 11/ex008.js
@@ -0,0 +1,8 @@
+let v = 60.5
+console.log (`A velocidade do seu carro é ${v}km/h`)
+
+if(v > 60){
+    console.log(`Você ultrapassou a velocidade permitida.`)
+}
+
+console.log(`Sempre dirija com sinto de segurança.`)
